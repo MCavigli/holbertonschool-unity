77d5d21b3eda5eb3d0d28bf76315aa45de2ac01a
Marc <mcavigli@gmail.com>
2020-03-23 13:09:30 -0700

    Added timer functionality

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+	public Text TimerText;
+	float intTime;
+	float mSeconds;
+	int seconds;
+	int minutes;
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		UpdateTimer();
+	}
+
+	void UpdateTimer()
+	{
+		intTime += Time.deltaTime;
+		minutes = (int)intTime / 60;
+		seconds = (int)intTime % 60;
+		mSeconds = Time.deltaTime * 1000;
+		mSeconds %= 1000;
+		TimerText.text = string.Format("{0:0}:{1:00}.{2:00}", minutes, seconds, mSeconds);
+	}
+}
---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/TimerTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerTrigger : MonoBehaviour
+{
+	public GameObject player;
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.name == "Player")
+		{
+			player.GetComponent<Timer>().enabled = true;
+		}
+	}
+}
