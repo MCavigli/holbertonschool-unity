17bce9308b15b68a68f0f23102596c5f85e8d784
Marc <mcavigli@gmail.com>
2020-04-04 13:35:25 -0700

    Added two new scenes

---
+++ 0x06-unity-assets_ui/Assets/Scripts/TreeTipper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeTipper : MonoBehaviour
+{
+	public GameObject player;
+	public GameObject tree;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.name == "Player")
+		{
+
+		}
+	}
+}
---
+++ 0x06-unity-assets_ui/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTrigger : MonoBehaviour
+{
+	public GameObject player;
+	public Text timerText;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.name == "Player")
+		{
+			player.GetComponent<Timer>().enabled = false;
+			timerText.fontSize = 60;
+			timerText.color = Color.green;
+		}
+	}
+}
