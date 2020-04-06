aaf45de1018a8e8edc66b2b2b31a2b4d7fc956e3
Marc <mcavigli@gmail.com>
2020-04-06 14:06:42 -0700

    Added scripts for main menu and options menu

---
+++ 0x06-unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MainMenu : MonoBehaviour
+{
+	// void Start()
+	// {
+	// 	var aScene = SceneManager.GetActiveScene().buildIndex;
+	// }
+	void Update()
+	{
+		// if (Input.GetMouseButtonDown(0))
+		// {
+		// Debug.Log(aScene);
+		// Debug.Log(EventSystem.current.currentSelectedGameObject.name);
+		// }
+		if (EventSystem.current.currentSelectedGameObject.name == "Level01")
+			LevelSelect(0);
+		if (EventSystem.current.currentSelectedGameObject.name == "Level02")
+			LevelSelect(1);
+		if (EventSystem.current.currentSelectedGameObject.name == "Level03")
+			LevelSelect(2);
+		if (EventSystem.current.currentSelectedGameObject.name == "OptionsButton")
+			Options();
+		if (EventSystem.current.currentSelectedGameObject.name == "ExitButton")
+		{
+			Application.Quit();
+			Debug.Log("Exited");
+		}
+	}
+	public void LevelSelect(int level)
+	{
+		SceneManager.LoadScene(level);
+	}
+	public void Options()
+	{
+
+		SceneManager.LoadScene(4);
+	}
+}
---
+++ 0x06-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class OptionsMenu : MonoBehaviour
+{
+
+	void Update()
+	{
+		if (EventSystem.current.currentSelectedGameObject.name == "BackButton")
+			Back();
+	}
+	public void Back()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+	}
+}
