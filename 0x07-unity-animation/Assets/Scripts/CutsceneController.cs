4ba7211368c3ebd0bba9e8f7a499950b3c5babcc
Marc <mcavigli@gmail.com>
2020-04-20 12:08:07 -0700

    Activates game objects after animation plays so player cannot move during animation

---
+++ 0x07-unity-animation/Assets/Scripts/CutsceneController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneController : MonoBehaviour
+{
+
+	Animator ani;
+	public GameObject mCam;
+	public GameObject player;
+	public GameObject timer;
+	public GameObject csCont;
+
+	void Awake()
+	{
+		ani = GetComponent<Animator>();
+	}
+	void Transition()
+	{
+		mCam.SetActive(true);
+		timer.SetActive(true);
+		csCont.SetActive(false);
+		player.GetComponent<PlayerController>().enabled = true;
+	}
+}
