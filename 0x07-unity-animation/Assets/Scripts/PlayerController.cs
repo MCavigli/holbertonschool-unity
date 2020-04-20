58e3bce089b20e2300819e4a4b1297a463c1cbb3
Marc <mcavigli@gmail.com>
2020-04-20 13:02:48 -0700

    Changed player movement code

--- 0x07-unity-animation/Assets/Scripts/PlayerController.cs
+++ 0x07-unity-animation/Assets/Scripts/PlayerController.cs
@@ -15,11 +15,16 @@ public class PlayerController : MonoBehaviour
 	{
 		pos = GetComponent<Transform>();
 	}
+
 	void Update()
 	{
-		float x = Input.GetAxis("Horizontal");
-		float z = Input.GetAxis("Vertical");
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
 
+		// This causes the character to face the correct direction
+		// but it breaks jumping
+		// if (moveHorizontal == 0 && moveVertical == 0)
+		// 	return;
 
 		if (cc.isGrounded && velocity.y < 0)
 		{
@@ -29,17 +34,49 @@ public class PlayerController : MonoBehaviour
 		{
 			velocity.y = Mathf.Sqrt(jump * -2f * gravity);
 		}
-
-		Vector3 move = transform.right * x + transform.forward * z;
-
-		cc.Move(move * speed * Time.deltaTime);
+		Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
 
 		velocity.y += gravity * Time.deltaTime;
-
 		cc.Move(velocity * Time.deltaTime);
+
+
+		if (movement != Vector3.zero)
+		{
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement.normalized), 0.2f);
+		}
+		transform.Translate(movement * speed * Time.deltaTime, Space.World);
 		if (pos.position.y < -30f)
 		{
 			pos.position = new Vector3(0, 50, 0);
 		}
 	}
+	// Original movement code
+	// void Update()
+	// {
+	// 	float x = Input.GetAxis("Horizontal");
+	// 	float z = Input.GetAxis("Vertical");
+
+
+	// 	if (cc.isGrounded && velocity.y < 0)
+	// 	{
+	// 		velocity.y = -2f;
+	// 	}
+	// 	if (Input.GetButtonDown("Jump") && cc.isGrounded)
+	// 	{
+	// 		velocity.y = Mathf.Sqrt(jump * -2f * gravity);
+	// 	}
+
+	// 	Vector3 move = transform.right * x + transform.forward * z;
+
+	// 	cc.Move(move * speed * Time.deltaTime);
+
+	// 	velocity.y += gravity * Time.deltaTime;
+
+	// 	cc.Move(velocity * Time.deltaTime);
+	// 	if (pos.position.y < -30f)
+	// 	{
+	// 		pos.position = new Vector3(0, 50, 0);
+	// 	}
+	// }
 }
