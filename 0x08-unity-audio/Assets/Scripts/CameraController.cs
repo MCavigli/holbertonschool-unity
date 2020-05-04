64f0f26e09d41c3c1a988befefa9ca632934d416
Marc <mcavigli@gmail.com>
2020-05-04 10:46:02 -0700

    Initial push

---
+++ 0x08-unity-audio/Assets/Scripts/CameraController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	float turnSpeed = 90f;
+	float upperAngleLimit = 50f;
+	float lowerAngleLimit = -25f;
+	float yaw = 0f;
+	float pitch = 0f;
+	float distanceZ = 5.0f;
+	float distanceY = 2.0f;
+	public Transform lookAt;
+	public bool isInverted;
+	Camera cam;
+
+	void Start()
+	{
+		if (PlayerPrefs.GetString("Inverted") != "")
+			isInverted = false;
+		else
+			isInverted = true;
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+		cam = Camera.main;
+	}
+
+	void FixedUpdate()
+	{
+		float horizontal = Input.GetAxis("Mouse X") * Time.deltaTime * turnSpeed;
+		float vertical = Input.GetAxis("Mouse Y") * Time.deltaTime * turnSpeed;
+
+		yaw += horizontal;
+		pitch += vertical;
+		pitch = Mathf.Clamp(pitch, lowerAngleLimit, upperAngleLimit);
+
+		Vector3 dir = new Vector3(0, distanceY, -distanceZ);
+		Quaternion rotation = Quaternion.Euler(pitch, yaw, 0f);
+		transform.localPosition = lookAt.position + rotation * dir;
+		transform.LookAt(lookAt.position);
+	}
+
+
+
+
+	// First attempt
+	// // public GameObject player;
+	// // public Vector3 offset;
+	// public float mouseSensitivity = 150f;
+
+	// public Transform lookAt;
+	// public Transform camTransform;
+
+	// public bool isInverted;
+
+	// private Camera cam;
+	// private float distanceZ = 5.0f;
+	// private float distanceY = 2.0f;
+	// private float currentX = 0f;
+	// private float currentY = 0f;
+	// // public Transform camBody;
+	// // float xRotation = 0f;
+
+	// void Start()
+	// {
+	// 	if (PlayerPrefs.GetString("Inverted") != "")
+	// 		isInverted = false;
+	// 	else
+	// 		isInverted = true;
+	// 	// int isInverted = PlayerPrefs.GetInt("Inverted");
+	// 	Cursor.lockState = CursorLockMode.Locked;
+	// 	camTransform = transform;
+	// 	cam = Camera.main;
+	// }
+
+	// void Update()
+	// {
+	// 	currentX += Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+	// 	currentY += Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime * (isInverted ? -1 : 1);
+	// 	currentY = Mathf.Clamp(currentY, -25f, 50f);
+
+	// }
+	// void LateUpdate()
+	// {
+	// 	Vector3 dir = new Vector3(0, distanceY, -distanceZ);
+	// 	Quaternion rotation = Quaternion.Euler(currentY, currentX, 0f);
+	// 	camTransform.position = lookAt.position + rotation * dir;
+	// 	camTransform.LookAt(lookAt.position);
+	// }
+}
---
+++ 0x08-unity-audio/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	float speed = 12f;
+	float jump = 2f;
+	float gravity = -20f;
+	float airControl = 5f;
+	float turnSpeed = 200f;
+	Vector3 moveDirection = Vector3.zero;
+	CharacterController cc;
+	Transform pos;
+	Vector3 startPos;
+	Animator anim;
+	// Vector3 velocity;
+
+	void Start()
+	{
+		cc = GetComponent<CharacterController>();
+		pos = GetComponent<Transform>();
+		anim = transform.GetChild(0).GetComponent<Animator>();
+		startPos = transform.position;
+	}
+	void FixedUpdate()
+	{
+		Vector3 input = new Vector3(
+			Input.GetAxis("Horizontal"),
+			0,
+			Input.GetAxis("Vertical")
+		);
+
+		input *= speed;
+		input = transform.TransformDirection(input);
+		if (cc.isGrounded)
+		{
+			if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+			{
+				anim.SetBool("runs", true);
+				if (Input.GetButton("Jump"))
+				{
+					anim.SetBool("runs", false);
+					anim.SetTrigger("jumps");
+				}
+			}
+			else
+			{
+				anim.SetBool("runs", false);
+			}
+
+			moveDirection = input;
+			// if (moveDirection.y < 0)
+			// 	moveDirection.y = -2f;
+			if (Input.GetButton("Jump"))
+			{
+				moveDirection.y = Mathf.Sqrt(-2 * gravity * jump);
+				anim.SetTrigger("jumps");
+			}
+			else
+			{
+				moveDirection.y = 0;
+			}
+		}
+		else
+		{
+			input.y = moveDirection.y;
+			moveDirection = Vector3.Lerp(moveDirection, input, airControl * Time.deltaTime);
+		}
+		float turn = Input.GetAxis("Horizontal");
+		transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
+		moveDirection.y += gravity * Time.deltaTime;
+		cc.Move(moveDirection * Time.deltaTime);
+		if (pos.position.y < -10f)
+			anim.SetTrigger("falling");
+
+		if (pos.position.y < -95f)
+		{
+			pos.position = new Vector3(startPos.x, 30, startPos.z);
+		}
+
+	}
+
+
+
+
+	// Second attempt
+	// void Update()
+	// {
+	// 	float moveHorizontal = Input.GetAxis("Horizontal");
+	// 	float moveVertical = Input.GetAxis("Vertical");
+	// 	Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+	// 	if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+	// 	{
+	// 		anim.SetTrigger("runs");
+	// 	}
+
+	// 	// This causes the character to face the correct direction
+	// 	// but it breaks jumping
+	// 	// if (moveHorizontal == 0 && moveVertical == 0)
+	// 	// 	return;
+
+	// 	if (cc.isGrounded && velocity.y < 0)
+	// 	{
+	// 		velocity.y = -2f;
+	// 	}
+	// 	if (Input.GetButtonDown("Jump") && cc.isGrounded)
+	// 	{
+	// 		velocity.y = Mathf.Sqrt(jump * -2f * gravity);
+	// 		anim.SetTrigger("jumps");
+	// 	}
+
+
+	// 	// Makes model face the direction they're moving
+	// 	transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+
+	// 	velocity.y += gravity * Time.deltaTime;
+	// 	cc.Move(velocity * Time.deltaTime);
+
+	// 	if (movement != Vector3.zero)
+	// 	{
+	// 		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement.normalized), 0.2f);
+	// 		anim.SetTrigger("idle");
+	// 	}
+
+	// 	// Allows model to move in world
+	// 	transform.Translate(movement * speed * Time.deltaTime, Space.World);
+
+	// 	if (pos.position.y < -30f)
+	// 	{
+	// 		pos.position = new Vector3(0, 50, 0);
+	// 		anim.SetTrigger("falling");
+	// 	}
+	// }
+
+
+
+
+	// Original movement code
+	// void Update()
+	// {
+	// 	float x = Input.GetAxis("Horizontal");
+	// 	float z = Input.GetAxis("Vertical");
+
+
+	// 	if (cc.isGrounded && velocity.y < 0)
+	// 	{
+	// 		velocity.y = -2f;
+	// 	}
+	// 	if (Input.GetButtonDown("Jump") && cc.isGrounded)
+	// 	{
+	// 		velocity.y = Mathf.Sqrt(jump * -2f * gravity);
+	// 	}
+
+	// 	Vector3 move = transform.right * x + transform.forward * z;
+
+	// 	cc.Move(move * speed * Time.deltaTime);
+
+	// 	velocity.y += gravity * Time.deltaTime;
+
+	// 	cc.Move(velocity * Time.deltaTime);
+	// 	if (pos.position.y < -30f)
+	// 	{
+	// 		pos.position = new Vector3(0, 50, 0);
+	// 	}
+	// }
+}
