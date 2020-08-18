3c6193a7c8b0fffee5157dba3aa351ecf6e11abf
Marc <mcavigli@gmail.com>
2020-08-18 16:22:53 -0700

    Initial push

---
+++ 0x0C-unity-ar_slingshot_game/Assets/Scripts/AmmoFunc.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.Audio;
+
+/// <summary>
+/// The game's functionality. WIP!
+/// </summary>
+public class AmmoFunc : MonoBehaviour
+{
+	// PRIVATE VARIABLES
+
+	/// <summary>
+	/// Raycast manager component
+	/// </summary>
+	ARRaycastManager m_RaycastManager;
+
+	/// <summary>
+	/// Reference manager componenet
+	/// </summary>
+	ARReferencePointManager m_ReferencePoint;
+
+	/// <summary>
+	/// List that holds the results of a raycast hit
+	/// </summary>
+	List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+	/// <summary>
+	/// Predefined pose to make sure the ammo is offset to the camera
+	/// </summary>
+	Pose ammoPose = new Pose(new Vector3(0.0f, 0.0f, 0.3f), Quaternion.identity);
+
+	/// <summary>
+	/// The center position of the ammo. This needs work!
+	/// </summary>
+	Vector3 centerPos;
+
+	/// <summary>
+	/// A reference to this script
+	/// </summary>
+	AmmoFunc ammoFuncScript;
+
+	/// <summary>
+	/// A reference to selectPlane script
+	/// </summary>
+	SelectPlane selectPlaneScript;
+
+	/// <summary>
+	/// A flag that detects when the player is out of ammo
+	/// </summary>
+	bool outOfAmmo = false;
+
+	/// <summary>
+	/// A general flag that makes a reference point and populates a piece of ammo
+	/// </summary>
+	int flag = 0;
+
+	/// <summary>
+	/// The player's score
+	/// </summary>
+	int score = 0;
+
+	// PUBLIC VARIABLES
+
+	/// <summary>
+	/// The GameObject representation of ammo taken from the reference pointf
+	/// </summary>
+	public GameObject ammo;
+
+	/// <summary>
+	/// Game UI for different scenarios
+	/// </summary>
+	public GameObject gameCanvas;
+	public GameObject quitCanvas;
+	public GameObject replayCanvas;
+
+	/// <summary>
+	/// Each bullet in the ammo count
+	/// </summary>
+	public GameObject bullet1;
+	public GameObject bullet2;
+	public GameObject bullet3;
+	public GameObject bullet4;
+	public GameObject bullet5;
+	public GameObject bullet6;
+	public GameObject bullet7;
+
+	/// <summary>
+	/// Displays player's score
+	/// </summary>
+	public Text scoreText;
+
+	/// <summary>
+	/// The prefab to spawn at the reference point
+	/// </summary>
+	public ARReferencePoint spawnedObjectRef;
+
+	/// <summary>
+	/// A reference to the main camera
+	/// </summary>
+	public Camera mainCamera;
+
+	public AudioSource introMusic;
+	public AudioSource ammoPickup;
+	public AudioSource ammoDrop;
+	public AudioSource gameOver;
+
+	/// <summary>
+	/// Component references and scripts get initialized
+	/// </summary>
+	void Awake()
+	{
+		m_RaycastManager = GetComponent<ARRaycastManager>();
+		m_ReferencePoint = GetComponent<ARReferencePointManager>();
+		ammoFuncScript = GetComponent<AmmoFunc>();
+		selectPlaneScript = GetComponent<SelectPlane>();
+		introMusic.Play();
+	}
+
+	/// <summary>
+	/// Runs once per frame
+	/// </summary>
+	void FixedUpdate()
+	{
+		// Checks to see if player is out of ammo. If so, triggers the replay canvas
+		if (outOfAmmo)
+		{
+			gameOver.Play();
+			replayCanvas.SetActive(true);
+		}
+
+		// Runs only once to make the reference point for the ammo the player interacts with.
+		// It sets the spawned prefab as a child of the camera, then we save that child as a
+		// game object - ammo. Center position of ammo is also set.
+		if (flag == 0)
+		{
+			gameCanvas.SetActive(true);
+			spawnedObjectRef = m_ReferencePoint.AddReferencePoint(ammoPose);
+			spawnedObjectRef.transform.SetParent(mainCamera.transform, false);
+			ammo = mainCamera.gameObject.transform.GetChild(0).gameObject;
+			centerPos = spawnedObjectRef.transform.position;
+			flag = 1;
+		}
+
+		// Detects if player touches the screen.
+		if (spawnedObjectRef && Input.touchCount > 0)
+		{
+			ammoPickup.Play();
+			if ((Input.GetTouch(0).phase == TouchPhase.Moved))
+			{
+				Touch touch = Input.GetTouch(0);
+
+				if (m_RaycastManager.Raycast(touch.position, s_Hits, TrackableType.FeaturePoint))
+				{
+					ammo.transform.position = s_Hits[0].pose.position + Vector3.zero;
+				}
+			}
+
+			// Occurs when player releases the ammo
+			if (Input.GetTouch(0).phase == TouchPhase.Ended)
+			{
+				ammoDrop.Play();
+				ammo.transform.position = centerPos;
+				scoreText.text = "Score: " + score.ToString();
+				score += 5;
+				if (bullet7.activeSelf)
+					bullet7.SetActive(false);
+				else if (bullet6.activeSelf)
+					bullet6.SetActive(false);
+				else if (bullet5.activeSelf)
+					bullet5.SetActive(false);
+				else if (bullet4.activeSelf)
+					bullet4.SetActive(false);
+				else if (bullet3.activeSelf)
+					bullet3.SetActive(false);
+				else if (bullet2.activeSelf)
+					bullet2.SetActive(false);
+				else if (bullet1.activeSelf)
+				{
+					bullet1.SetActive(false);
+					outOfAmmo = true;
+				}
+			}
+		}
+	}
+
+	/// <summary>
+	/// Restarts the game by reseting everything score and ammo count
+	/// </summary>
+	public void restartGame()
+	{
+		scoreText.text = "Score: 0";
+		score = 0;
+		bullet1.SetActive(true);
+		bullet2.SetActive(true);
+		bullet3.SetActive(true);
+		bullet4.SetActive(true);
+		bullet5.SetActive(true);
+		bullet6.SetActive(true);
+		bullet7.SetActive(true);
+		outOfAmmo = false;
+		if (replayCanvas.activeSelf)
+			replayCanvas.SetActive(false);
+	}
+
+	/// <summary>
+	/// Disaplays the quit game canvas
+	/// </summary>
+	public void quitGame()
+	{
+		gameCanvas.SetActive(false);
+		quitCanvas.SetActive(true);
+	}
+
+	/// <summary>
+	/// Quits the game if the player confirms they want to quit
+	/// </summary>
+	public void quitYes()
+	{
+		Application.Quit();
+	}
+
+	/// <summary>
+	/// Goes back to the game if the user decides they don't want to quit
+	/// </summary>
+	public void quitNo()
+	{
+		gameCanvas.SetActive(true);
+		quitCanvas.SetActive(false);
+	}
+}
---
+++ 0x0C-unity-ar_slingshot_game/Assets/Scripts/SelectPlane.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Collections;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class SelectPlane : MonoBehaviour
+{
+	ARPlaneManager m_PlaneManager;
+	ARRaycastManager m_RaycastManager;
+	float i = 0.07f;
+	float k = -0.07f;
+
+	AmmoFunc ammoFuncScript;
+	SelectPlane selectPlaneScript;
+
+	static ARPlane chosenPlane;
+	static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+	static int flag;
+
+	public int numberOfTargets;
+	public GameObject spawnedObject { get; private set; }
+	public GameObject startButton;
+	public GameObject placedPrefab;
+
+	void Awake()
+	{
+		m_RaycastManager = GetComponent<ARRaycastManager>();
+		m_PlaneManager = GetComponent<ARPlaneManager>();
+		ammoFuncScript = GetComponent<AmmoFunc>();
+		selectPlaneScript = GetComponent<SelectPlane>();
+		flag = 0;
+	}
+
+	void FixedUpdate()
+	{
+		if (Input.touchCount > 0 && flag == 0)
+		{
+			Touch touch = Input.GetTouch(0);
+			if (touch.phase == TouchPhase.Ended)
+			{
+				if (Input.touchCount == 1)
+				{
+					if (m_RaycastManager.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
+					{
+						chosenPlane = m_PlaneManager.GetPlane(s_Hits[0].trackableId);
+						Vector3 newCenter = new Vector3(chosenPlane.center.x, chosenPlane.center.y, chosenPlane.center.z);
+						spawnedObject = Instantiate(placedPrefab, newCenter, Quaternion.identity);
+						for (int j = 1; j < numberOfTargets; i += 0.07f, j++, k -= 0.07f)
+						{
+							spawnedObject = Instantiate(placedPrefab, newCenter + new Vector3(i, 0, k), Quaternion.identity);
+						}
+						foreach (var plane in m_PlaneManager.trackables)
+						{
+							plane.gameObject.SetActive(false);
+						}
+						startGame();
+						flag = 1;
+					}
+				}
+			}
+		}
+	}
+	void startGame()
+	{
+		startButton.SetActive(true);
+	}
+
+	public void populatePlane()
+	{
+		startButton.SetActive(false);
+		ammoFuncScript.enabled = true;
+		selectPlaneScript.enabled = false;
+	}
+}
